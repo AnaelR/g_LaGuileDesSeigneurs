0218ec4996b9cb081581c105bbfcc40a80798139
AnaëlR <hey@anael-rodet.fr>
2021-11-08 14:02:07 +0100

    Update test

--- tests/CharacterControllerTest.php
+++ tests/CharacterControllerTest.php
@@ -6,14 +6,15 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class CharacterControllerTest extends WebTestCase
 {
+
+    private $client;
     /**
      * Test index
      */
     public function testIndex(): void
     {
-        $client = static::createClient();
-        $client->request('GET', '/character/index');
-        $this->assertJsonResponse($client->getResponse());
+        $this->client->request('GET', '/character/index');
+        $this->assertJsonResponse();
     }
 
     /**
@@ -21,25 +22,62 @@ class CharacterControllerTest extends WebTestCase
      */
     public function testDisplay()
     {
-        $client = static::createClient();
-        $client->request('GET', '/character/display/2cfd170cee19ff797265df8844c576c0e38aa630');
+        $this->client->request('GET', '/character/display/2cfd170cee19ff797265df8844c576c0e38aa630');
 
-        $this->assertJsonResponse($client->getResponse());
+        $this->assertJsonResponse();
     }
 
     /**
      * Assert that a Response is in json
      */
-    public function assertJsonResponse($response)
+    public function assertJsonResponse()
     {
+        $response = $this->client->getResponse();
         $this->assertEquals(200, $response->getStatusCode());
         $this->assertTrue($response->headers->contains('Content-Type', 'application/json'), $response->headers);
     }
 
-    public function testRedirectIndex(){
-        $client = static::createClient();
-        $client->request('GET', '/character');
+    /**
+     * Test de redirection
+     */
+    public function testRedirectIndex()
+    {
+        $this->client->request('GET', '/character');
 
-        $this->assertEquals(302, $client->getResponse()->getStatusCode());
+        $this->assertEquals(302, $this->client->getResponse()->getStatusCode());
     }
+
+    /**
+     * Create client
+     */
+    public function setUp(): void
+    {
+        $this->client = static::createClient();
+    }
+
+    /**
+     * test mauvais identifier
+     */
+    public function testBadidentifier()
+    {
+        $this->client->request('GET', '/character/display/badIdentifier');
+        $this->assertError404();
+    }
+
+    /**
+     * test error 404
+     */
+    public function assertError404()
+    {
+        $this->assertEquals(404, $this->client->getResponse()->getStatusCode());
+    }
+
+    /**
+     * test identifiant non existant
+     */
+    public function testInexistingIdentifier(){
+        $this->client->request('GET', '/character/display/error');
+        $this->assertError404();
+    }
+
 }
